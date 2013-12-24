2e0f3f63863d7f19859115f4d26724b2e06f71a8
Daniel Luque <danielluque14@gmail.com>
2013-12-24 04:57:03 +0100

    Add license to ninja_error_log.py
    Fixed #4

--- ninja_error_log/ninja_error_log.py
+++ ninja_error_log/ninja_error_log.py
@@ -1,3 +1,20 @@
+# -*- coding: UTF-8 -*-
+# This file is part of Ninja-error-log
+# Ninja-error-log is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# Ninja-error-log is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ninja-error-log. If not, see <http://www.gnu.org/licenses/>.
+#
+# Source url (https://github.com/LuqueDaniel/Ninja-error-log)
+
 #Ninja_ide imports
 from ninja_ide.core import plugin
 
