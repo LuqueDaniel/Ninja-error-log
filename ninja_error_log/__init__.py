0f567d72af209fb99700e8fde0a6c18370d5cc03
Daniel Luque <danielluque14@gmail.com>
2013-12-24 05:10:28 +0100

    Add license to ninja_error_log/__init__.py
    Fixed #3

--- ninja_error_log/__init__.py
+++ ninja_error_log/__init__.py
@@ -1,9 +1,26 @@
-from ninja_error_log import ninjaErrorLog
+# -*- coding: UTF-8 -*-
+# This file is part of Ninja-error-log
+# Ninja-error-log is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# Ninja-error-log is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ninja-error-log. If not, see <http://www.gnu.org/licenses/>.
+#
+# Source url (https://github.com/LuqueDaniel/Ninja-error-log)
+
+from .ninja_error_log import ninjaErrorLog
 
 
 __author__ = 'Daniel Luque'
 __mail__ = 'danielluque14@gmail.com'
 __url__ = 'https://github.com/LuqueDaniel/Ninja-error-log'
-__version__ = '0.1'
+__version__ = '0.2'
 __source__ = 'https://github.com/LuqueDaniel/Ninja-error-log'
 __license__ = 'GPLv3'
