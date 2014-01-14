9e1b958ea3ea908c3b34b4f5dfdd5f1289c7b822
Daniel Luque <danielluque14@gmail.com>
2014-01-14 10:31:22 +0100

    Small changes in to_zip_file.py

--- to_zip_file.py
+++ to_zip_file.py
@@ -1,10 +1,29 @@
-import ninja_error_log
+# -*- coding: UTF-8 -*-
+#
+# This file is part of Ninja-error-log
+# Ninja-error-log is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# Ninja-error-log is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ninja-error-log. If not, see <http://www.gnu.org/licenses/>.
+#
+# Source url (https://github.com/LuqueDaniel/Ninja-error-log)
+
 import zipfile
 import os
 import re
 
-folders = ['ninja_error_log']
-path_list = ['ninja_error_log.plugin']
+VERSION = "0.2"
+
+FOLDERS = ['ninja_error_log']
+PATH_LIST = ['ninja_error_log.plugin']
 
 
 def get_file_list(path):
@@ -13,13 +32,13 @@ def get_file_list(path):
         if os.path.isdir(item_src):
             get_file_list(item_src)
         elif os.path.isfile(item_src) and not re.search('.+\.pyc$', item):
-            path_list.append(item_src)
+            PATH_LIST.append(item_src)
 
 
-with zipfile.ZipFile('ninja-eror-log_{}.zip'.format(ninja_error_log.__version__), 'w') as zip_file:
-    for folder in folders:
+with zipfile.ZipFile('ninja-eror-log_{}.zip'.format(VERSION), 'w') as zip_file:
+    for folder in FOLDERS:
         get_file_list(os.path.join(folder))
-    for item in path_list:
+    for item in PATH_LIST:
         zip_file.write(item)
 
     zip_file.close()
